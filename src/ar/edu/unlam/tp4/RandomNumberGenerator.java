2cdfaeb0fadd52d69121ec710e6c5aecfd18c12a
Fernando <fernando.rthea@gmail.com>
2016-06-15 17:13:47 -0300

    Fix

--- src/ar/edu/unlam/tp4/RandomNumberGenerator.java
+++ src/ar/edu/unlam/tp4/RandomNumberGenerator.java
@@ -4,7 +4,7 @@ public class RandomNumberGenerator implements NumberGenerator {
 
 	@Override
 	public Integer generate() {
-		return (int)(Math.random() * (10 - 1) + 1 )  + 1;
+		return (int)(Math.random() * 10 )  + 1;
 	}
 
 }
