6147825db15c52490f9a1d32eb9902835c5845de
Fernando <fernando.rthea@gmail.com>
2016-06-21 00:26:21 -0300

    Se agregó el test al tambor.

--- Test/ar/edu/unlam/tp4/TestTragamonedas.java
+++ Test/ar/edu/unlam/tp4/TestTragamonedas.java
@@ -40,6 +40,21 @@ public class TestTragamonedas {
 		System.out.println("Primer tambor: " + primerPosicion);
 		System.out.println("\nSegundo  tambor: " + segundaPosicion);
 		System.out.println("\nTercer tambor: " + terceraPosicion);
+		
+		assertTrue( (primerPosicion >= 1 && primerPosicion <= 10) &&
+				(segundaPosicion >= 1 && segundaPosicion <= 10) &&
+				(terceraPosicion >= 1 && terceraPosicion <= 10));
+	}
+	
+	@Test
+	public void queElTamborFuncioneCorrectamente(){ 
+		/* Creo un objeto con un numero al azar (en este caso 3),
+		 * inicializo el tambor en esa posicion y verifico
+		 * que se haya iniciado correctamente
+		 */
+		FixNumberGenerator numero = new FixNumberGenerator(3);
+		Tambor nuevoTambor = new Tambor(numero);
+		assertEquals(numero.generate(), nuevoTambor.getPosicion());
 	}
 
 }
